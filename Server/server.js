86c2c140e04423efdd84fd775870c4374de58eae
Elena Frees <elfr20ab@student.cbs.dk>
2020-11-21 21:43:07 +0100

    Sat op til local storage

--- Server/server.js
+++ Server/server.js
@@ -1,21 +1,29 @@
-//API
-const User = require('../Client/class')
+const express = require('express');
+const server = express();
+const PORT = process.env.port || 4000;
 
-server.post("/class", (req, res) => {
+//API
+const User = require('../Client/model/class.js')
+
+server.post("/class.js", (req, res) => {
  res.json(user[0], "Match has been created")
 });
 
 
-server.get("/class", (req, res) => {
+server.get("/class.js", (req, res) => {
  res.json(user[0])
 });
 
 
-server.put("/class", (req, res) => {
+server.put("/class.js", (req, res) => {
  res.json(user[0], "Match has been updated")
 });
 
 
-server.delete("/class", (req, res) => {
+server.delete("/class.js", (req, res) => {
  res.json(user[0], "Match has been deleted")
 });
+
+server.listen(PORT, () => {
+    console.log(`Server-applikation lytter på http://localhost:${PORT}`)
+  })
